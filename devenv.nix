e15d25354dfb2877fd7b8d34b95fc800b30199b8
Safenein <safenein@cavernum.ovh>
2026-01-22 01:33:59 +0100

    fix: adding secretspecs

--- devenv.nix
+++ devenv.nix
@@ -7,8 +7,21 @@
 }:
 
 {
+  # https://devenv.sh/basics/
+  env.BARENTSWATCH_API_TOKEN = config.secretspec.secrets.BARENTSWATCH_AIS_TOKEN or "";
+
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [pkgs.git pkgs.secretspec];
+
+  enterShell = ''
+    BARENTSWATCH_AIS_TOKEN="$(curl -X POST https://id.barentswatch.no/connect/token \
+       -H 'Content-Type: application/x-www-form-urlencoded' \
+       --data-urlencode 'client_id=safenein@cavernum.ovh:nova' \
+       --data-urlencode "client_secret=$(pass show barents_watch/api)" \
+       --data-urlencode 'scope=ais' \
+       --data-urlencode 'grant_type=client_credentials' | jq -r '.access_token')"
+    export BARENTSWATCH_AIS_TOKEN
+  '';
 
   # https://devenv.sh/languages/
   languages.python = {
@@ -22,6 +35,9 @@
 
   # https://devenv.sh/services/
   services.nats.enable = true;
+  # processes = {
+  #   "norway-api-emitter:dev".exec = "ls *.py | entr 'uv run main.py'";
+  # };
 
   # https://devenv.sh/tasks/
   tasks = {
